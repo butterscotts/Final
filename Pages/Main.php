85a054e7c148e4fa04a68ecad7a0a6596f2e09f9
butterscotts <36580591+butterscotts@users.noreply.github.com>
2019-02-01 11:04:10 -0500

    Update Main.php

--- Pages/Main.php
+++ Pages/Main.php
@@ -8,6 +8,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+         <link href="https://fonts.googleapis.com/css?family=Orbitron|ZCOOL+QingKe+HuangYou" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=ZCOOL+QingKe+HuangYou" rel="stylesheet">
         <title>
             Home
         </title>
@@ -19,54 +21,292 @@
             overflow-x: hidden;
         }
         .button{
-	background-color:#12abe3;
-	-moz-border-radius:28px;
-	-webkit-border-radius:28px;
-	border-radius:28px;
-	/*border:1px solid #000000;*/
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	padding:16px 31px;
-	text-decoration:none;
-	text-shadow:0px 1px 0px #457585;
-	 font-size: 25px;
-          right: 30%;
-          top: 50%;
-          position: fixed;
-           width: 15%;
-          height: 20%;
-                } 
+	        position: absolute;
+            top: 40%;
+            left: 25%;
+            width: 20%;
+            color: white;
+            font-size: 25px;
+            font-weight: bold;
+            height: 16%;
+            background: rgba(255, 0, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(200,50,0,0.5), inset 0 0 8px 2px rgba(200,50,0,0.5);
+        } 
+        .button:hover {
+            background: rgba(255, 0, 0, 0.8);
+            box-shadow: 0 0 8px 2px rgba(255,0,0,1.0), inset 0 0 8px 2px rgba(255,0,0,1.0);
+        }
+        .button1 {
+        	position: absolute;
+            top: 40%;
+            right: 25%;
+            width: 20%;
+            height: 16%;
+            color: white;
+            font-size: 25px;
+            font-weight: bold;
+            background: rgba(100, 0, 255, 0.3);
+            box-shadow: 0 0 8px 2px rgba(100,0,255,0.5), inset 0 0 8px 2px rgba(100,0,255,0.5);
+        }
+        .button1:hover {
+	        background: rgba(100, 0, 255, 0.8);
+            box-shadow: 0 0 8px 2px rgba(100,0,255,1.0), inset 0 0 8px 2px rgba(100,0,255,1.0);
+        }
+         .box1{
+        
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 20%;
+            height: 25%;
+            background: rgba(255, 0, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,0,0,0.5), inset 0 0 8px 2px rgba(255,0,0,0.5);
+            
+        
+        }
+        .box2{
+    
+            position: absolute;
+            bottom: 14%;
+            left: 12%;
+            width: 15%;
+            height: 18%;
+            background: rgba(0, 255, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(0,255,0,0.5), inset 0 0 8px 2px rgba(0,255,0,0.5);
+        }
+        .box3{
+      
+            position: absolute;
+            bottom: 0;
+            left: 20%;
+            width: 15%;
+            height: 16%;
+            background: rgba(0, 0, 255, 0.3);
+            box-shadow: 0 0 8px 2px rgba(0,0,255,0.5), inset 0 0 8px 2px rgba(0,0,255,0.5);
+            
+ 
+        }
+        .box4{
+         
+            position: absolute;
+            bottom: 0;
+            left: 33%;
+            width: 6%;
+            height: 22%;
+            background: rgba(255, 0, 255, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,0,255,0.5), inset 0 0 8px 2px rgba(255,0,255,0.5);
+            
+    
+        }
+        .box5{
+   
+            position: absolute;
+            bottom: 25%;
+            left: 0;
+            width: 18%;
+            height: 15%;
+            background: rgba(0, 0, 255, 0.3);
+            box-shadow: 0 0 8px 2px rgba(0,0,255,0.5), inset 0 0 8px 2px rgba(0,0,255,0.5);
+            
+        }
+        .box6{
+            position: absolute;
+            bottom: 38%;
+            left: 0;
+            width: 15%;
+            height: 15%;
+            background: rgba(255, 0, 255, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,0,255,0.5), inset 0 0 8px 2px rgba(255,0,255,0.5);
 
-.button:hover {
-	background-color:#00d0f0;
-}
+        }
+        .box7{
 
-.button1 {
-	background-color:#12abe3;
-	-moz-border-radius:28px;
-	-webkit-border-radius:28px;
-	border-radius:28px;
-	/*border:1px solid #000000;*/
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	padding:16px 31px;
-	text-decoration:none;
-	text-shadow:0px 1px 0px #457585;
-		 font-size: 25px;
-          left: 30%;
-          top: 50%;
-          position: fixed;
-          width: 15%;
-          height: 20%;
-}
-.button1:hover {
-	background-color:#00d0f0;
-}
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 20%;
+            height: 16%;
+            background: rgba(255, 150, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,150,0,0.5), inset 0 0 8px 2px rgba(255,150,0,0.5);
+ 
+        }
+        /*.box8{*/
+        /*    opacity: .3;*/
+        /*    position: absolute;*/
+        /*    bottom: 45%;*/
+        /*    right: 0;*/
+        /*    width: 20%;*/
+        /*    height: 6%;*/
+        /*    background: black;*/
+ 
+        /*}*/
+        /*.box9{*/
+        /*    opacity: .3;*/
+        /*    position: absolute;*/
+        /*    bottom: 49%;*/
+        /*    right: 0;*/
+        /*    width: 25%;*/
+        /*    height: 4%;*/
+        /*    background: white;*/
+ 
+        /*}*/
+        /*.box10{*/
+        /*    opacity: .3;*/
+        /*    position: absolute;*/
+        /*    bottom: 45%;*/
+        /*    right: 15%;*/
+        /*    width: 5%;*/
+        /*    height: 13%;*/
+        /*    background: red;*/
+ 
+        /*}*/
+        .box11{
 
+            position: absolute;
+            bottom: 0;
+            left: 50%;
+            width: 15%;
+            height: 7%;
+            background: rgba(0, 0, 255, 0.3);
+            box-shadow: 0 0 8px 2px rgba(0,0,255,0.5), inset 0 0 8px 2px rgba(0,0,255,0.5);
+   
+            
+ 
+        }
+        .box12{
+
+            position: absolute;
+            bottom: 0;
+            left: 43%;
+            width: 15%;
+            height: 10%;
+            background: rgba(255, 0, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,0,0,0.5), inset 0 0 8px 2px rgba(255,0,0,0.5);
+        }
+        .box13{
+
+            position: absolute;
+            bottom: 0;
+            left: 60%;
+            width: 7%;
+            height: 12%;
+            background: rgba(255, 140, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,140,0,0.5), inset 0 0 8px 2px rgba(255,140,0,0.5);
+            
+ 
+        }
+        .box14{
+            
+            position: absolute;
+            bottom: 0;
+            left: 70;
+            width: 7%;
+            height: 10%;
+            background: rgba(100, 255, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(100,255,0,0.5), inset 0 0 8px 2px rgba(100,255,0,0.6);
+            
+            
+ 
+        }
+        .box15{
+            position: absolute;
+            bottom: 0;
+            left: 67%;
+            width: 5%;
+            height: 7%;
+            background: rgba(255, 255, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,255,0,0.5), inset 0 0 8px 2px rgba(255,255,0,0.5);
+            
+ 
+        }
+        .box16{
+            position: absolute;
+            bottom: 0;
+            left: 38%;
+            width: 10%;
+            height: 15%;
+            background: rgba(255, 255, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,255,0,0.5), inset 0 0 8px 2px rgba(255,255,0,0.5);
+            
+ 
+        }
+        .box17{
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            width: 20%;
+            height: 10%;
+            background: rgba(255, 255, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,255,0,0.5), inset 0 0 8px 2px rgba(255,255,0,0.5);
+            
+ 
+        }
+        .box18{
+            position: absolute;
+            bottom: 0;
+            right: 10%;
+            width: 15%;
+            height: 22%;
+            background: rgba(100, 255, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(100,255,0,0.5), inset 0 0 8px 2px rgba(100,255,0,0.6);
+            
+ 
+        }
+        .box19{
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            width: 12%;
+            height: 25%;
+            background: rgba(0, 0, 255, 0.3);
+            box-shadow: 0 0 8px 2px rgba(0,0,255,0.5), inset 0 0 8px 2px rgba(0,0,255,0.6);
+            
+ 
+        }
+        .box20{
+            position: absolute;
+            bottom: 0;
+            right: 18%;
+            width: 14%;
+            height: 15%;
+            background: rgba(255, 0, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,0,0,0.5), inset 0 0 8px 2px rgba(255,0,0,0.6);
+            
+ 
+        }
+        .box21{
+            position: absolute;
+            bottom: 18%;
+            right: 0%;
+            width: 23%;
+            height: 10%;
+            background: rgba(0, 255, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(0,255,0,0.5), inset 0 0 8px 2px rgba(0,255,0,0.6);
+            
+ 
+        }
+        .box22{
+   
+            position: absolute;
+            bottom: 28%;
+            right: 0;
+            width: 15%;
+            height: 12%;
+            background: rgba(255, 150, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,150,0,0.5), inset 0 0 8px 2px rgba(255,150,0,0.5);
+            
+        }
+        .box23{
+   
+            position: absolute;
+            bottom: 35%;
+            right: 0;
+            width: 10%;
+            height: 15%;
+            background: rgba(255, 0, 0, 0.3);
+            box-shadow: 0 0 8px 2px rgba(255,0,0,0.5), inset 0 0 8px 2px rgba(255,0,0,0.5);
+            
+        }
         .title{
             color: white;
             font-size: 525%;
@@ -75,6 +315,8 @@
             margin-right: -50%;
             transform: translate(-50%, -50%);
             position: fixed;
+            font-family: 'ZCOOL QingKe HuangYou', cursive;
+            font-family: 'Orbitron', sans-serif;
             text-align: center;
 }
 .description{
@@ -86,12 +328,10 @@
             transform: translate(-50%, -50%);
             position: fixed;
         }
-
         </style>
          <?php
          
   //recalls the Session variable created in CheckLogin.php to obtain the user's id number and to confirm they have logged in.
-
         	if (isset($_SESSION['user'])) {
           $user = $_SESSION['user'];
           $stat = $_SESSION['stat'];
@@ -109,7 +349,28 @@
     <body> 
     
         <a href="https://mrticket-mrcalihan.c9users.io/Index.php"><span class="glyphicon glyphicon-log-out"></span> Log Out</a>
-        
+            <div class= "box5"></div>
+            <div class= "box3"></div>
+            <div class= "box7"></div>
+            <div class= "box2"></div>
+            <div class= "box20"></div>
+            <div class= "box1"></div>
+            <div class= "box8"></div>
+            <div class= "box9"></div>
+            <div class= "box10"></div>
+            <div class= "box11"></div>
+            <div class= "box12"></div>
+            <div class= "box13"></div>
+            <div class= "box14"></div>
+            <div class= "box15"></div>
+            <div class= "box23"></div>
+            <div class= "box21"></div>
+            <div class= "box16"></div>
+            <div class= "box17"></div>
+            <div class= "box18"></div>
+            <div class= "box4"></div>
+            <div class= "box19"></div>
+            <div class= "box22"></div>
             <div class="title" id="quoteDisplay">
             </div>
               <button class="button" onclick = "Javascript:window.location.href = '/Pages/Profile.php';">
@@ -118,7 +379,8 @@
               <button class="button1" onclick = "Javascript:window.location.href = '/Pages/TicketWindow.php';">
               Place a <br> new ticket
               </button>
-
+                <div class= "box6"></div>
+            
             <!--<button onclick="newQuote()">New Quote</button>-->
             <script type="text/javascript">
             window.onload = newQuote;
